d3ab90e6d9f6bb0a958893fd6be8e2712d339b52
Alina3a <alina2baddies1porche@gmail.com>
2024-06-01 00:05:45 +0300

    fix logo image

--- src/comp/Header/Burger/Burger.jsx
+++ src/comp/Header/Burger/Burger.jsx
@@ -7,7 +7,7 @@ const Burger = () => {
         <div class="hamburger-menu">
     <input id="menu__toggle" type="checkbox" />
     <label class="menu__btn" for="menu__toggle">
-        <img className="logo" src='https://yandex.ru/images/search?pos=11&from=tabbar&text=logo&img_url=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F3%2F30%2FVector-based_example.svg%2F440px-Vector-based_example.svg.png&lr=10472&rpt=simage'></img>
+        <img className="logo" src='https://i.pinimg.com/originals/d0/b3/46/d0b346b2e7073bc5e24e372453df7dab.png' width="35"></img>
       <span></span>
     </label>
 
